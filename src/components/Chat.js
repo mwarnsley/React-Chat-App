9ee7596da486a6b461db6a563bf03a2d0430b9e4
Marcus Warnsley <marcus@PC0159-Jscott.local>
2017-09-29 22:28:45 -0400

    Editing the initial header with the title

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
+import {Grid, Row, PageHeader} from 'react-bootstrap';
 
 class Chat extends Component {
   render() {
-    return <div>CHATTER</div>;
+    return (
+      <div id="chat_app_container">
+        <div className="header-container">
+          <h1 className="chatter-box-title-container">
+            <i className="fa fa-comments chatter-logo" aria-hidden="true" />
+            <span className="chatter-box-title">Chatter Box</span>
+          </h1>
+        </div>
+        <Grid />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Users/UserContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class UserContainer extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default UserContainer;
